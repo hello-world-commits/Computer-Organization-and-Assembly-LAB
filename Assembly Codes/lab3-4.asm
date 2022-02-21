c1be7ca9f759906da2f85259bcb84f670e157044
Muhammad Talha <80144916+hello-world-commits@users.noreply.github.com>
2022-02-21 13:00:48 +0500

    Add files via upload

---
+++ Assembly Codes/lab3-4.asm
@@ -0,0 +1,25 @@
+[org 0x0100]
+
+;for (int i =3 ; i > 0 ; i++){
+    ; result += data[i]
+
+
+  mov ax, 0    ;reset accumulator
+  mov cx, 3    ; set iteration count
+  mov bx, num1 ; set the base
+
+  outerloop:
+    add ax, [bx]
+    add bx, 2
+
+    sub cx, 1
+
+    jnz outerloop 
+
+    mov [result], ax
+
+    mov ax,0x4c00
+    int 0x21
+
+num1 : dw 5,10,15
+result : dw 0
