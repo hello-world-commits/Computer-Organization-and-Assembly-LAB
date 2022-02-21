fb79cd47d9cdd027aaee72744a20da53094900c8
Muhammad Talha <80144916+hello-world-commits@users.noreply.github.com>
2022-02-21 12:35:27 +0500

    Add files via upload

---
+++ Assembly Codes/lab3-3.asm
@@ -0,0 +1,20 @@
+[org 0x0100]
+
+xor ax, ax     ;make sure ax is empty
+
+add ax,[bx]
+add bx,2
+
+add ax,[bx]
+add bx,2
+
+add ax,[bx]
+add bx,2
+
+mov [result], ax
+
+mov ax, 0x4c00
+int 0x21
+
+num1 : dw 5, 10, 15
+result : dw 0
